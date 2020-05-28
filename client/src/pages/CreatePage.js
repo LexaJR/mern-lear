97c100dde8636fdedb0b79917f73a2d595d56123
LexaJR <39440669+LexaJR@users.noreply.github.com>
2020-05-29 00:12:14 +0700

    middleware auth
    added middleware

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import 'materialize-css'
 
 export const CreatePage = () => {
     return(
-        <div>
-            <h1>Create page</h1>
-        </div>
+        <div></div>
     )
 
 }
