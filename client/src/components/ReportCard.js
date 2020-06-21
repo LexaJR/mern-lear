1fdc00a2aad11b107cddc5d5786470455bb04ece
LexaJR <39440669+LexaJR@users.noreply.github.com>
2020-06-21 11:49:14 +0700

    create report card

---
+++ client/src/components/ReportCard.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useCallback } from "react"
+import { useMessage } from "../hooks/message.hook"
+//import {useHistory} from 'react-router-dom'
+import { useHttp } from "../hooks/http.hook"
+
+export const ReportCard = ({ report }) => {
+  
+  return (
+    <div></div>
+  )
+}
